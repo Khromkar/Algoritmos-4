38b7b6c639c6673a586998d8d8b6de165a565c44
Steve <khromkar@proton.me>
2026-02-23 09:36:29 -0500

    Se agregan ejemplos con strings y números

--- Repo Compartido/recursion.py
+++ Repo Compartido/recursion.py
@@ -23,12 +23,12 @@ def recursion(someValue):
         - Can lead to out of memory errors / stack overflow exceptions
         - Can be unnecesarily complex if poorly constructed
 """
-
-# Recursion with strings
-
-# Example - String Reversal: Input a string, output the string backwards
-#                              "Hello" -> "olleH"
-
+#########################################################################
+#                   ****RECURSION WITH STRINGS****                      #
+#########################################################################
+# Example - String Reversal: Input a string, output the string backwards#
+#                              "Hello" -> "olleH"                       #
+#########################################################################
 def reverseString(input):
     # What is the base case? || When can I no longer continue?
     """
@@ -44,9 +44,63 @@ def reverseString(input):
     """
         How do we even get to the point where we pass in an empty string?
     """
+    #reverseString(input[1:]) shrinks the problem space, as I'm passing in a shorter string
+    #input[0] is the smallest unit of work to contribute to the recursive call
     return reverseString(input[1:]) + input[0]
 
-print(reverseString("Hola"))
+# print(reverseString("Hola"))
+# reverseString("ola") + H
+# reverseString ("la") + o
+# reverseString ("a") + l
+# reverseString ("") + a <- which reaches the base case
+# returns aloH
 
+#########################################################################
+# Example - Palindrome: Input a string, check if its spelled the same   #
+#                       backwards than forward                          #
+#                    -> "kayak" backwards is "kayak"                    #
+#########################################################################
 
+def palindrome(word):
+    # Base case. One or none letters left:
+    if len(word) == 0 or len(word) == 1:
+        return True
+    
+    # Shrink problem space
+    if word[0] == word[-1]:
+        return palindrome(word[1:-1])
+    # In case the word is not palindrome
+    else:
+        return False
+
+# print(palindrome("kayak"))
+# as k = k, return palindrome("aya")
+# as a = a, return palindrome("y")
+# as y is 1 letter long, we reach base case, returns True
+
+#########################################################################
+#                   ****RECURSION WITH NUMBERS****                      #
+#########################################################################
+# Example - Decimal to Binary: Input a number, return its decimal value #
+#########################################################################
+
+def binaryConverter(number,result):
+    # Base case: number//2 = 0
+    if (number // 2) == 0:
+        return result
+    
+    # Shrink problem space
+    else:
+        result = str(number % 2) + result
+        return binaryConverter(number//2,result)
+
+print(binaryConverter(233,""))
+# 233 // 2 is not 0 -> result = "1" + "" -> return binaryConverter(116,"1")
+# 116 // 2 is not 0 -> result = "0" + "1" -> return binaryConverter(58,"01")
+# 58 // 2 is not 0 -> result = "0" + "01" -> return binaryConverter(29 + "001")
+# 29 // 2 is not 0 -> result = "1" + "001" -> return binaryConverter(14 + "1001")
+# 14 // 2 is not 0 -> result = "0" + "1001" -> return binaryConverter(7 + "01001")
+# 7 // 2 is not 0 -> result = "1" + "01001" -> return binaryConverter(3 + "101001")
+# 3 // 2 is not 0 -> result = "1" + "101001" -> return binaryConverter(1 + "1101001")
+# 1 // 2 is 0 -> return "1101001"
 
